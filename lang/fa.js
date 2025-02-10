919b7ecc97e75a9905a91223d9c835dcebc13d8a
Qihua Peng <50785121+qihuapengclimate@users.noreply.github.com>
2025-02-09 22:42:30 -0800

    Update fa.js

--- lang/fa.js
+++ lang/fa.js
@@ -3,7 +3,7 @@ const faNavbarData = {
   Home: " 主页 ",
   publications: " 发表文章 ",
   Research: " 研究兴趣 ",
-  Jobs: " 工作招聘 ",
+  Jobs: " 相册 ",
   Contact: " 联系方式 ",
 };
 
@@ -11,85 +11,211 @@ const faHomePageData = {
   name: "  彭启华 ",
   jobTitle: " 加州大学圣地亚哥斯克里普斯海洋研究所博士后 ",
   home_title: " 关于我 ",
-  home_content: ` <p> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد </p>
-    <h2 class='title'> کارهای من </h2>
-    <p> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد. </p>
+  home_content: `
+    <div>
+    <p>I am a postdoctoral researcher at Scripps Institution of Oceanography, UC San Diego. My academic journey began with a B.Sc. in Ocean Sciences from Nanjing University’s School of Physics (July 2013), leading to a Ph.D. in Physical Oceanography from the South China Sea Institute of Oceanology, Chinese Academy of Sciences (July 2019). Between 2016 and 2018, I conducted research as a joint Ph.D. student at Scripps Institution of Oceanography.</li> 
+    <p>  </p>
+    I am interested in the dynamics of atmospheric circulation and ocean-atmosphere interaction. My research focuses on the climate dynamics, emphasizing tropical air-sea interactions, ocean current variability and their changes under global warming.  I typically employ a combination of observational data analysis, realistic numerical experiments, and diagnostic approaches to investigate these topics.
+    <h2 class='title'>  </h2>
+    <p>  </p>
     <ul>
-      <li> عنوان تستی </li>
-      <li>  عنوان تستی  2  </li>
-      <li> عنوان تستی 3  </li>
+      <li> Air-sea interaction</li>
+      <li> Ocean dynamics </li>
+      <li> El Niño-Southern Oscillation (ENSO) </li>
+      <li> Climate change </li>      
     </ul>
+  </div>
   `,
 };
 
 const faPublicationsPageData = {
-  type_one_title: "مقالات منتشر شده",
+  type_one_title: "发表的文章",
 
   type_one_items: [
     {
-      title: `رقصیدن با ضربان تغییرات اقلیمی: مطالعه فریم‌های بصری در TikTok از طریق شکاف چپ و راست میان سیاستمداران ایالات متحده (با ). `,
-      abstract: `سیاستمداران در سراسر طیف سیاسی در تلاش برای دسترسی به رای دهندگان جوان با تولید ویدیوهای اصلی به TikTok رفته اند. این روند جدید، TikTok را به حلقه قطبی جدید در نبرد ارتباطات سیاسی تبدیل کرده است و نفوذ حزبی در مورد موضوعاتی مانند تغییرات آب و هوا را عمیق تر می کند. با این حال `,
+      title: `Peng, Q., Xie, S.-P., & Deser, C., (2024). Collapsed upwelling weakens ENSO under sustained warming beyond the 21st century. Nature Climate Change, 14(8), 815-822.`,
+      abstract: ``,
       date: "",
-      link: "http://link.com",
+      link: "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=kAKvj4IAAAAJ&sortby=pubdate&citation_for_view=kAKvj4IAAAAJ:_kc_bZDykSQC",
       github: "",
-      writers: ["علی احمدی", " سارا امینی "],
+      writers: [],
     },
     {
-      title: ` "اکنون شما به زبان من صحبت می کنید" شفافیت ویژه زبان و مذاکرات قانونی در شورای وزیران اتحادیه اروپا. (با ).`,
-      abstract: `سیاستمداران در سراسر طیف سیاسی در تلاش برای دسترسی به رای دهندگان جوان با تولید ویدیوهای اصلی به TikTok رفته اند. این روند جدید، TikTok را به حلقه قطبی جدید در نبرد ارتباطات سیاسی تبدیل کرده است و نفوذ حزبی در مورد موضوعاتی مانند تغییرات آب و هوا را عمیق تر می کند. با این حال `,
-      date: "1996",
-      link: "",
-      github: "http://github.com",
-      writers: ["علی احمدی"],
+      title: `Peng, Q., Xie, S.-P., Passalacqua, G., Miyamoto A., & Deser, C., (2024). The 2023 extreme coastal El Niño: Atmospheric and air-sea coupling mechanisms. Science Advances, 10(12).`,
+      abstract: ``,
+      date: "",
+      link: "https://www.science.org/doi/full/10.1126/sciadv.adk8646",
+      github: "",
+      writers: [],
     },
+    {
+      title: `Peng, Q., Xie, S.-P., Huang R., Wang, W., Zu, T., & Wang, D. (2023). Indonesian Throughflow slowdown under global warming: Remote AMOC effect vs. regional surface forcing. Journal of Climate, 36(5),1301-1318.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/clim/36/5/JCLI-D-22-0331.1.xml",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Wang, D., Huang R., Chen G., Shu Y., et al. (2022). Surface warming–induced global acceleration of upper ocean currents. Science Advances, 8(16). `,
+      abstract: ``,
+      date: "",
+      link: "https://www.science.org/doi/full/10.1126/sciadv.abj8394",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Wang, D., Kamae, Y., Zhang, H., Hu, S., et al. (2020). Eastern Pacific wind effect on the evolution of El Niño: Implications for ENSO diversity. Journal of Climate, 33(8), 3197-3212.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/clim/33/8/jcli-d-19-0435.1.xml",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Wang, D., Zheng, X.-T., & Zhang, H. (2019). Coupled ocean-atmosphere dynamics of the 2017 extreme coastal El Niño. Nature Communications, 10(1), 298.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41467-018-08258-8",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Xie, S.-P., Peng, Q.#(co-first author), Kamae, Y., Zheng, X.-T., Tokinaga, H., & Wang, D. (2018). Eastern Pacific ITCZ dipole and ENSO diversity. Journal of Climate, 31(11), 4449-4462.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/clim/31/11/jcli-d-17-0905.1.xml?tab_body=fulltext-display",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Huang, R.-X., Wang, W., & Wang, D. (2020). The main heaving modes in the Pacific Ocean. Journal of Tropical Oceanography, 39(2), 1-10.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.jto.ac.cn/EN/Y2020/V39/I2/1",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Luo, X., Yang, L., Chan, J.C., Chen, S., Peng, Q., & Wang, D., (2024). China coasts facing more tropical cyclone risks during the second decaying summer of double-year La Niña events. npj Climate and Atmospheric Science, 7(1), 198.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41612-024-00729-8",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Zhou, X., Zhou, W., Wang, D., Xie, Q., Yang, L. and Peng, Q., (2024). Westerlies Affecting the Seasonal Variation of Water Vapor Transport over the Tibetan Plateau Induced by Tropical Cyclones in the Bay of Bengal. Advances in Atmospheric Sciences, 41(5), 881-893.`,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s00376-023-3093-7",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Wang, S., Jing, Z., Wu, L., Sun, S., Peng, Q., Wang, H., et al. (2023). Southern hemisphere eastern boundary upwelling systems emerging as future marine heatwave hotspots under greenhouse warming. Nature Communications, 14(1), 28.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41467-022-35666-8",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Li, Y., Xie, S.P., Lian, T., Zhang, G., Feng, J., Ma, J., Peng, Q., Wang, W., Hou, Y. and Li, X., (2023). Interannual variability of regional Hadley circulation and El Niño interaction. Geophysical Research Letters, 50(4), e2022GL102016.`,
+      abstract: ``,
+      date: "",
+      link: "https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2022GL102016",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Chu, X., & Peng, Q. (2023). The role of alongshore wind and ocean wave in generating the northward Somali Current. Journal of Tropical Oceanography, 42(2), 1-8.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.jto.ac.cn/EN/PDF/10.11978/2022055",
+      github: "",
+      writers: [],
+    }, 
+    {
+      title: `Chen, G., Huang, R. X., Peng, Q., & Chu, X. (2022). A time-dependent Sverdrup relation and its application to the Indian Ocean. Journal of Physical Oceanography, 52(6), 1233-1244.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/phoc/52/6/JPO-D-21-0223.1.xml",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Liang, Z., Zeng, L., Wang, Q., Peng, Q., & Wang, D. (2022). Interpretation of interannual variability of the zonal contrasting thermal conditions in the winter South China Sea. Climate Dynamics, 1-19.`,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s00382-021-05968-6",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Zhang, Y., Yu, S.-Y., Xie, S.-P., Amaya, D. J., Peng, Q., Kosaka, Y., et al. (2022). Role of ocean dynamics in equatorial Pacific decadal variability. Climate Dynamics, 59(7-8), 2517-2529. `,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s00382-022-06312-2",
+      github: "",
+      writers: [],
+    }, 
+    {
+      title: `Zhong, Q., Chen, G., Li, Y., Peng, Q., & Chu, X. (2022). Intraseasonal variability of the surface zonal current in the equatorial Indian Ocean: Seasonal differences and causes. Acta Oceanologica Sinica, 41(5), 12-26.`,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s13131-021-1935-7",
+      github: "",
+      writers: [],
+    },  
+    {
+      title: `Shi, J.-R., Talley, L. D., Xie, S.-P., Peng, Q., & Liu, W. (2021). Ocean warming and accelerating Southern Ocean zonal flow. Nature Climate Change. `,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41558-021-01212-5",
+      github: "",
+      writers: [],
+    },  
+    {
+      title: `Zeng, L., Chen, G., Huang, K., Chen, J., He, Y., Zhou, F., Yang, Y., Liang, Z., Peng, Q., Shi, R. and Gamage, T.P., (2021). A decade of eastern tropical Indian Ocean Observation Network (TIOON). Bulletin of the American Meteorological Society, 102(10), E2034-E2052.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/bams/102/10/BAMS-D-19-0234.1.xml",
+      github: "",
+      writers: [],
+    },      
   ],
-
-  type_two_title: "مقالات تحت بررسی ",
+  type_two_title: "Under Review",
   type_two_items: [
     {
-      title: `رقصیدن با ضربان تغییرات اقلیمی: مطالعه فریم‌های بصری در TikTok از طریق شکاف چپ و راست میان سیاستمداران ایالات متحده (با ). `,
-      abstract: `سیاستمداران در سراسر طیف سیاسی در تلاش برای دسترسی به رای دهندگان جوان با تولید ویدیوهای اصلی به TikTok رفته اند. این روند جدید، TikTok را به حلقه قطبی جدید در نبرد ارتباطات سیاسی تبدیل کرده است و نفوذ حزبی در مورد موضوعاتی مانند تغییرات آب و هوا را عمیق تر می کند. با این حال `,
+      title: `Peng, Q., Xie, S.-P., Miyamoto A., Deser, C., P. Zhang, & Luongo M., Unusual growth of the 2023-24 El Niño against the odds of Indo-Atlantic warming. Nature Geoscience (Accepted).`,
+      abstract: ``,
       date: "",
-      link: "http://link.com",
+      link: "",
       github: "",
       writers: [],
     },
     {
-      title: ` "اکنون شما به زبان من صحبت می کنید" شفافیت ویژه زبان و مذاکرات قانونی در شورای وزیران اتحادیه اروپا. (با ).`,
-      abstract: `سیاستمداران در سراسر طیف سیاسی در تلاش برای دسترسی به رای دهندگان جوان با تولید ویدیوهای اصلی به TikTok رفته اند. این روند جدید، TikTok را به حلقه قطبی جدید در نبرد ارتباطات سیاسی تبدیل کرده است و نفوذ حزبی در مورد موضوعاتی مانند تغییرات آب و هوا را عمیق تر می کند. با این حال `,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["علی احمدی", " سارا امینی "],
-    },
-  ],
-
-  type_three_title: "فصل های کتاب ",
-  type_three_items: [
-    {
-      title: `رقصیدن با ضربان تغییرات اقلیمی: مطالعه فریم‌های بصری در TikTok از طریق شکاف چپ و راست میان سیاستمداران ایالات متحده (با ). `,
-      abstract: `سیاستمداران در سراسر طیف سیاسی در تلاش برای دسترسی به رای دهندگان جوان با تولید ویدیوهای اصلی به TikTok رفته اند. این روند جدید، TikTok را به حلقه قطبی جدید در نبرد ارتباطات سیاسی تبدیل کرده است و نفوذ حزبی در مورد موضوعاتی مانند تغییرات آب و هوا را عمیق تر می کند. با این حال `,
+      title: `Luongo M., Xie, S.-P., Eisenman I., & Peng, Q.,. How the Subsurface Tropical Pacific Responds to Hemispherically Asymmetric Temperature Forcing: Implications for Cross-Equatorial Mass and Heat Transport. Journal of Climate (Minor revision).`,
+      abstract: ``,
       date: "",
-      link: "http://link.com",
+      link: "",
       github: "",
       writers: [],
     },
-    {
-      title: ` "اکنون شما به زبان من صحبت می کنید" شفافیت ویژه زبان و مذاکرات قانونی در شورای وزیران اتحادیه اروپا. (با ).`,
-      abstract: `سیاستمداران در سراسر طیف سیاسی در تلاش برای دسترسی به رای دهندگان جوان با تولید ویدیوهای اصلی به TikTok رفته اند. این روند جدید، TikTok را به حلقه قطبی جدید در نبرد ارتباطات سیاسی تبدیل کرده است و نفوذ حزبی در مورد موضوعاتی مانند تغییرات آب و هوا را عمیق تر می کند. با این حال `,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["علی احمدی"],
+   {
+      title: `Taylor, B. A., Shi, J.-R., Xie, S.-P., Talley, L. D., Luongo, M. T., & Peng, Q. (2024). Warming band in Southern Ocean’s Indian sector: The role of remote Atlantic buoyancy forcing via poleward-shifting circulation response. Journal of Climate (second review) `,
+      abstract: ``,
+      date: "",
+      link: "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=kAKvj4IAAAAJ&sortby=pubdate&citation_for_view=kAKvj4IAAAAJ:_kc_bZDykSQC",
+      github: "",
+      writers: [],
     },
   ],
-
-  type_four_title: "",
-  type_four_items: [],
 };
 
 const faResearchPageData = {
-  title: " تحقیقات ",
+  title: "研究",
   content: `
     <div class='research_content'>
         <p>هدف تحقیق من ترکیب سه زیر زمینه اصلی علوم اجتماعی محاسباتی، از جمله تجزیه و تحلیل داده های رسانه های اجتماعی، تجزیه و تحلیل شبکه، و مدل سازی مبتنی بر عامل، برای مطالعه مسائل مختلف علوم اجتماعی است. در زیر می توانید خلاصه ای از پروژه های گذشته و فعلی من را مشاهده کنید. </p>
@@ -102,7 +228,7 @@ const faResearchPageData = {
 };
 
 const faJobsPageData = {
-  title: " سوابق شغلی ",
+  title: "相册",
   items: [
     {
       title: `عنوان شغلی `,
