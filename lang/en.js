06e690bec288cb3cb82a732c546cd182435c98a2
Qihua Peng <50785121+qihuapengclimate@users.noreply.github.com>
2025-02-09 22:03:34 -0800

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -43,58 +43,190 @@ const enPublicationsPageData = {
       github: "",
       writers: [],
     },
-
+    {
+      title: `Peng, Q., Xie, S.-P., Passalacqua, G., Miyamoto A., & Deser, C., (2024). The 2023 extreme coastal El Niño: Atmospheric and air-sea coupling mechanisms. Science Advances, 10(12).`,
+      abstract: ``,
+      date: "",
+      link: "https://www.science.org/doi/full/10.1126/sciadv.adk8646",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Huang R., Wang, W., Zu, T., & Wang, D. (2023). Indonesian Throughflow slowdown under global warming: Remote AMOC effect vs. regional surface forcing. Journal of Climate, 36(5),1301-1318.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/clim/36/5/JCLI-D-22-0331.1.xml",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Wang, D., Huang R., Chen G., Shu Y., et al. (2022). Surface warming–induced global acceleration of upper ocean currents. Science Advances, 8(16). `,
+      abstract: ``,
+      date: "",
+      link: "https://www.science.org/doi/full/10.1126/sciadv.abj8394",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Wang, D., Kamae, Y., Zhang, H., Hu, S., et al. (2020). Eastern Pacific wind effect on the evolution of El Niño: Implications for ENSO diversity. Journal of Climate, 33(8), 3197-3212.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/clim/33/8/jcli-d-19-0435.1.xml",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Xie, S.-P., Wang, D., Zheng, X.-T., & Zhang, H. (2019). Coupled ocean-atmosphere dynamics of the 2017 extreme coastal El Niño. Nature Communications, 10(1), 298.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41467-018-08258-8",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Xie, S.-P., Peng, Q.#(co-first author), Kamae, Y., Zheng, X.-T., Tokinaga, H., & Wang, D. (2018). Eastern Pacific ITCZ dipole and ENSO diversity. Journal of Climate, 31(11), 4449-4462.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/clim/31/11/jcli-d-17-0905.1.xml?tab_body=fulltext-display",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Peng, Q., Huang, R.-X., Wang, W., & Wang, D. (2020). The main heaving modes in the Pacific Ocean. Journal of Tropical Oceanography, 39(2), 1-10.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.jto.ac.cn/EN/Y2020/V39/I2/1",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Luo, X., Yang, L., Chan, J.C., Chen, S., Peng, Q., & Wang, D., (2024). China coasts facing more tropical cyclone risks during the second decaying summer of double-year La Niña events. npj Climate and Atmospheric Science, 7(1), 198.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41612-024-00729-8",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Zhou, X., Zhou, W., Wang, D., Xie, Q., Yang, L. and Peng, Q., (2024). Westerlies Affecting the Seasonal Variation of Water Vapor Transport over the Tibetan Plateau Induced by Tropical Cyclones in the Bay of Bengal. Advances in Atmospheric Sciences, 41(5), 881-893.`,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s00376-023-3093-7",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Wang, S., Jing, Z., Wu, L., Sun, S., Peng, Q., Wang, H., et al. (2023). Southern hemisphere eastern boundary upwelling systems emerging as future marine heatwave hotspots under greenhouse warming. Nature Communications, 14(1), 28.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41467-022-35666-8",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Li, Y., Xie, S.P., Lian, T., Zhang, G., Feng, J., Ma, J., Peng, Q., Wang, W., Hou, Y. and Li, X., (2023). Interannual variability of regional Hadley circulation and El Niño interaction. Geophysical Research Letters, 50(4), e2022GL102016.`,
+      abstract: ``,
+      date: "",
+      link: "https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2022GL102016",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Chu, X., & Peng, Q. (2023). The role of alongshore wind and ocean wave in generating the northward Somali Current. Journal of Tropical Oceanography, 42(2), 1-8.`,
+      abstract: ``,
+      date: "",
+      link: "https://www.jto.ac.cn/EN/PDF/10.11978/2022055",
+      github: "",
+      writers: [],
+    },
+    
+    {
+      title: `Chen, G., Huang, R. X., Peng, Q., & Chu, X. (2022). A time-dependent Sverdrup relation and its application to the Indian Ocean. Journal of Physical Oceanography, 52(6), 1233-1244.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/phoc/52/6/JPO-D-21-0223.1.xml",
+      github: "",
+      writers: [],
+    },
+    {
+      title: `Liang, Z., Zeng, L., Wang, Q., Peng, Q., & Wang, D. (2022). Interpretation of interannual variability of the zonal contrasting thermal conditions in the winter South China Sea. Climate Dynamics, 1-19.`,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s00382-021-05968-6",
+      github: "",
+      writers: [],
+    },
+     {
+      title: `Zhang, Y., Yu, S.-Y., Xie, S.-P., Amaya, D. J., Peng, Q., Kosaka, Y., et al. (2022). Role of ocean dynamics in equatorial Pacific decadal variability. Climate Dynamics, 59(7-8), 2517-2529. `,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s00382-022-06312-2",
+      github: "",
+      writers: [],
+    }, 
+    {
+      title: `Zhong, Q., Chen, G., Li, Y., Peng, Q., & Chu, X. (2022). Intraseasonal variability of the surface zonal current in the equatorial Indian Ocean: Seasonal differences and causes. Acta Oceanologica Sinica, 41(5), 12-26.`,
+      abstract: ``,
+      date: "",
+      link: "https://link.springer.com/article/10.1007/s13131-021-1935-7",
+      github: "",
+      writers: [],
+    },  
+    {
+      title: `Shi, J.-R., Talley, L. D., Xie, S.-P., Peng, Q., & Liu, W. (2021). Ocean warming and accelerating Southern Ocean zonal flow. Nature Climate Change. `,
+      abstract: ``,
+      date: "",
+      link: "https://www.nature.com/articles/s41558-021-01212-5",
+      github: "",
+      writers: [],
+    },  
+    {
+      title: `Zeng, L., Chen, G., Huang, K., Chen, J., He, Y., Zhou, F., Yang, Y., Liang, Z., Peng, Q., Shi, R. and Gamage, T.P., (2021). A decade of eastern tropical Indian Ocean Observation Network (TIOON). Bulletin of the American Meteorological Society, 102(10), E2034-E2052.`,
+      abstract: ``,
+      date: "",
+      link: "https://journals.ametsoc.org/view/journals/bams/102/10/BAMS-D-19-0234.1.xml",
+      github: "",
+      writers: [],
+    },      
   ],
 
   type_two_title: "Under Review",
   type_two_items: [
     {
-      title: `Peng, Q., Xie, S.-P., Miyamoto A., Deser, C., P. Zhang, & Luongo M., (2024). Unusual growth of the 2023-24 El Niño against the odds of Indo-Atlantic warming. Nature Geoscience (Accepted).`,
+      title: `Peng, Q., Xie, S.-P., Miyamoto A., Deser, C., P. Zhang, & Luongo M., Unusual growth of the 2023-24 El Niño against the odds of Indo-Atlantic warming. Nature Geoscience (Accepted).`,
       abstract: ``,
       date: "",
       link: "",
       github: "",
       writers: [],
     },
+    {
+      title: `Luongo M., Xie, S.-P., Eisenman I., & Peng, Q.,. How the Subsurface Tropical Pacific Responds to Hemispherically Asymmetric Temperature Forcing: Implications for Cross-Equatorial Mass and Heat Transport. Journal of Climate (Minor revision).`,
+      abstract: ``,
+      date: "",
+      link: "",
+      github: "",
+      writers: [],
+    },
+   {
+      title: `Taylor, B. A., Shi, J.-R., Xie, S.-P., Talley, L. D., Luongo, M. T., & Peng, Q. (2024). Warming band in Southern Ocean’s Indian sector: The role of remote Atlantic buoyancy forcing via poleward-shifting circulation response. Journal of Climate (second review) `,
+      abstract: ``,
+      date: "",
+      link: "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=kAKvj4IAAAAJ&sortby=pubdate&citation_for_view=kAKvj4IAAAAJ:_kc_bZDykSQC",
+      github: "",
+      writers: [],
+    },
   ],
 
-  type_three_title: "Book Chapters",
+  type_three_title: " ",
   type_three_items: [
     {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+      title: ` `,
       abstract: ``,
       date: "",
       link: "",
       github: "",
       writers: [],
-    },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla"],
-    },
-  ],
-
-  type_four_title: "",
-  type_four_items: [
-    {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
-      abstract: ``,
-      date: "",
-      link: "",
-      github: "",
-      writers: [],
-    },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla"],
     },
   ],
 };
@@ -117,26 +249,14 @@ const enResearchPageData = {
 // ---------- Jobs page data
 const enJobsPageData = {
   title: "Gallery",
-  items: [
-    {
-      title: `Job title`,
-      company: "Company name",
-      startData: "20 April 2019",
-      endDate: "20 April 2022",
-      location: "Berlin",
-      abstract:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
-      achievements: ["achievement 1", "achievement 2", "achievement 3"],
-    },
-    {
-      title: `Job title`,
-      company: "Company name",
-      startData: "20 April 2019",
-      endDate: "",
-      location: "Berlin",
-      abstract:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
-      achievements: ["achievement 1", "achievement 2"],
-    },
-  ],
+  content: `
+    <div class='research_content'>
+        <p> My research goal is to combine the three major sub-fields of the Computational Social Science, including Social Media Data Analysis, Network Analysis, and Agent-based Modeling, to study various social science problems. Below you can find a summary of my past and current projects.</p>
+        <br />
+        <h4 style="font-size: 1.1rem "> Identifying and Characterizing US Domestic Ideological Extremists </h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+        <img src='../files/images/research.png' />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra  accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat <br /> imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet <br /> lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+    </div>
+  `,
 };
