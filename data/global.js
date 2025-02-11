1c99f408910c683facca403fed3d86b9a8a38f84
Qihua Peng <50785121+qihuapengclimate@users.noreply.github.com>
2025-02-10 21:55:54 -0800

    Update global.js

--- data/global.js
+++ data/global.js
@@ -21,8 +21,8 @@ const globalData = {
   googleScholar: "https://scholar.google.com/citations?user=kAKvj4IAAAAJ&hl=en",
   linkedinTitle: "Linkedin",
   linkedin: "https://www.linkedin.com/in/qihua-peng-179b30226/",
-  githubTitle: "",
-  github: "",
+  githubTitle: "CV",
+  github: "../files/cv.pdf",
   orcidTitle: "Orcid",
   orcid: "https://orcid.org/0000-0003-4913-3499",
 };
