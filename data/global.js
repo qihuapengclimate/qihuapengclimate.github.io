2ec8b32e95fd4030a4c44fe49898205f1a31414f
Qihua Peng <50785121+qihuapengclimate@users.noreply.github.com>
2025-02-10 21:59:50 -0800

    Update global.js

--- data/global.js
+++ data/global.js
@@ -13,6 +13,8 @@ const globalData = {
   faAddress: "Scripps Institution of Oceanography University of California, San Diego 9500 Gilman Drive, MC 206 La Jolla, CA 92093-0206",
   phone: "8582578423",
   email: "q2peng@ucsd.edu or qihpeng@hotmail.com",
+  githubTitle: "My Curriculum Vitae",
+  github: "../files/cv.pdf",
   twitterTitle: "Twitter",
   twitter: "https://x.com/generalpeng1",
   whatsappTitle: "",
@@ -21,8 +23,6 @@ const globalData = {
   googleScholar: "https://scholar.google.com/citations?user=kAKvj4IAAAAJ&hl=en",
   linkedinTitle: "Linkedin",
   linkedin: "https://www.linkedin.com/in/qihua-peng-179b30226/",
-  githubTitle: "CV",
-  github: "../files/cv.pdf",
   orcidTitle: "Orcid",
   orcid: "https://orcid.org/0000-0003-4913-3499",
 };
